30be0383851bed8a857ddd5ff9ed452d211ffc91
Kseniia <rydaya97@gmail.com>
2026-01-20 20:34:50 +0000

    added fonts

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,7 +4,7 @@ const testBar = ["test", "test1", "test2"];
 export default function Sidebar() {
   return (
     <div className="w-64 bg-white shadow-lg p-4 flex flex-col gap-3">
-      <h2 className="font-bold text-lg mb-4">Snippets</h2>
+      <h2 className="font-gothic font-bold text-lg mb-4">Snippets</h2>
       {testBar.map((item) => (
         <p
           key={`${item}`}
