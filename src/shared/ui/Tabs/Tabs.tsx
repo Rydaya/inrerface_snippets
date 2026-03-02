f755048fad337a31038f93a3c5db8b39dbe3d241
Kseniia <rydaya97@gmail.com>
2026-03-02 14:44:22 +0000

    added tabs variant

--- src/shared/ui/Tabs/Tabs.tsx
+++ src/shared/ui/Tabs/Tabs.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { TabsProps } from "./Tabs.types";
 import { clsx } from "clsx";
 
-const Tabs: React.FC<TabsProps> = ({ items }) => {
+const Tabs: React.FC<TabsProps> = ({ items, withDescription }) => {
   const [activeId, setActiveId] = useState<string | number | null>(items[0].id);
 
   const toggleItem = (id: string | number) => {
@@ -17,23 +17,26 @@ const Tabs: React.FC<TabsProps> = ({ items }) => {
         {items?.map((item) => (
           <button
             onClick={() => toggleItem(item.id)}
-            className={clsx(
-              "flex-1 px-4 py-2 border-t border-l border-r rounded-tl-xl rounded-tr-xl",
-              {
-                "bg-accent border-accent text-button-textPimary":
-                  activeId === item.id,
-              },
-            )}
+            className={clsx("flex-1 px-4 py-2", {
+              "bg-accent border-accent text-button-textPimary":
+                activeId === item.id && withDescription,
+              "border-b-4 border-accent":
+                activeId === item.id && !withDescription,
+              "border-t border-l border-r rounded-tl-xl rounded-tr-xl":
+                withDescription,
+            })}
             key={item.id}
           >
             {item.name}
           </button>
         ))}
       </div>
-      <div className="h-[4px] bg-accent" />
-      <div className="p-5 border">
-        {activeItem && <p>{activeItem.description}</p>}
-      </div>
+      {withDescription && <div className="h-[4px] bg-accent" />}
+      {withDescription && (
+        <div className="p-5 border">
+          {activeItem && <p>{activeItem.description}</p>}
+        </div>
+      )}
     </div>
   );
 };
